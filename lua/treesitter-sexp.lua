8a66e94627d6a6a06590c0be66e332d6892a9e3b
Jason Paterson <jasonbipaterson@gmail.com>
2023-07-12 16:56:42 +0100

    Add set config to setup

--- lua/treesitter-sexp.lua
+++ lua/treesitter-sexp.lua
@@ -1,7 +1,17 @@
 local M = {}
 
+local function set_config(opts)
+  local config = require("treesitter-sexp.config")
+  if opts ~= nil then
+    for key, value in pairs(opts) do
+      config[key] = vim.tbl_extend("keep", value, config[key])
+    end
+  end
+end
+
 --- @param opts? TSSexpConfig
 function M.setup(opts)
+  set_config(opts)
   require("treesitter-sexp.mappings").set()
 end
 
--- lua/treesitter-sexp/operators.lua
+++ lua/treesitter-sexp/operators.lua
@@ -14,82 +14,71 @@ local utils = require "treesitter-sexp.utils"
 --- | "barf_left"
 --- | "barf_right"
 
---- @type {name: TSSexpOpName, desc: string, action: TSSexpAction, get_node: TSSexpGetNode}[]
-local operators = {
-  {
-    name = "swap_prev_elem",
+--- @class TSSexpOp
+--- @field desc string
+--- @field actions TSSexpAction
+--- @field get_node TSSexpGetNode
+
+M = {
+  swap_prev_elem = {
     desc = "Swap previous element",
     action = actions.swap_prev,
     get_node = utils.get_elem_node,
   },
-  {
-    name = "swap_next_elem",
+  swap_next_elem = {
     desc = "Swap next element",
     action = actions.swap_next,
     get_node = utils.get_elem_node,
   },
-  {
-    name = "swap_prev_form",
+  swap_prev_form = {
     desc = "Swap previous form",
     action = actions.swap_prev,
     get_node = utils.get_form_node,
   },
-  {
-    name = "swap_next_form",
+  swap_next_form = {
     desc = "Swap next form",
     action = actions.swap_next,
     get_node = utils.get_form_node,
   },
-  {
-    name = "promote_elem",
+  promote_elem = {
     desc = "Promote element",
     action = actions.promote,
     get_node = utils.get_elem_node,
   },
-  {
-    name = "promote_form",
+  promote_form = {
     desc = "Promote form",
     action = actions.promote,
     get_node = utils.get_form_node,
   },
-  {
-    name = "splice",
+  splice = {
     desc = "Splice element",
     action = actions.splice,
     get_node = utils.get_elem_node,
   },
-  {
-    name = "slurp_left",
+  slurp_left = {
     desc = "Slurp left",
     action = actions.slurp_left,
     get_node = utils.get_elem_node,
   },
-  {
-    name = "slurp_right",
+  slurp_right = {
     desc = "Slurp right",
     action = actions.slurp_right,
     get_node = utils.get_elem_node,
   },
-  {
-    name = "barf_left",
+  barf_left = {
     desc = "Barf left",
     action = actions.barf_left,
     get_node = utils.get_elem_node,
   },
-  {
-    name = "barf_right",
+  barf_right = {
     desc = "Barf right",
     action = actions.barf_right,
     get_node = utils.get_elem_node,
   },
 }
 
-local M = {}
-for _, operator in ipairs(operators) do
-  M[operator.name] = {
-    desc = operator.desc,
-  }
-  setmetatable(M[operator.name], {
+for key, operator in pairs(M) do
+  setmetatable(M[key], {
     __call = function()
       local node = operator.get_node()
       if node == nil then
