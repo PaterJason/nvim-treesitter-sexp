f93a358fbec3d02942857dbd8e81e6bed67edc29
Jason Paterson <jasonbipaterson@gmail.com>
2023-08-28 22:39:49 +0100

    Revert "Simplify clojure query"
    This reverts commit 66ab25701acb6c9f25983a25a6732c504362dcfd.

--- queries/clojure/sexp.scm
+++ queries/clojure/sexp.scm
@@ -1,2 +1,40 @@
-(_ open: _ @sexp.open (_)? @sexp.elem close: _ @sexp.close) @sexp.form
-(source (_)? @sexp.elem)
+([
+  ; atom-ish
+  (num_lit)
+  (kwd_lit)
+  (str_lit)
+  (char_lit)
+  (nil_lit)
+  (bool_lit)
+  (sym_lit)
+  ; basic collection-ish
+  (list_lit)
+  (map_lit)
+  (vec_lit)
+  ; dispatch reader macros
+  (set_lit)
+  (anon_fn_lit)
+  (regex_lit)
+  (read_cond_lit)
+  (splicing_read_cond_lit)
+  (ns_map_lit)
+  (var_quoting_lit)
+  (sym_val_lit)
+  (evaling_lit)
+  (tagged_or_ctor_lit)
+  ; some other reader macros
+  (derefing_lit)
+  (quoting_lit)
+  (syn_quoting_lit)
+  (unquote_splicing_lit)
+  (unquoting_lit)
+  ] @sexp.elem)
+
+(anon_fn_lit            open: _ @sexp.open close: _ @sexp.close) @sexp.form
+(list_lit               open: _ @sexp.open close: _ @sexp.close) @sexp.form
+(map_lit                open: _ @sexp.open close: _ @sexp.close) @sexp.form
+(ns_map_lit             open: _ @sexp.open close: _ @sexp.close) @sexp.form
+(read_cond_lit          open: _ @sexp.open close: _ @sexp.close) @sexp.form
+(set_lit                open: _ @sexp.open close: _ @sexp.close) @sexp.form
+(splicing_read_cond_lit open: _ @sexp.open close: _ @sexp.close) @sexp.form
+(vec_lit                open: _ @sexp.open close: _ @sexp.close) @sexp.form
