eac8eaccdb3b3b04048bf0de52a7a97f658f85d8
Tyler Wardhaugh <twardhaugh@cap-rx.com>
2024-03-11 13:21:54 -0700

    rename sequence node following new fennel parser
    The nvim-treesitter repo recently switched fennel parsers[1]. The new
    parser[2] defines "sequence" in its grammer for sequence literals,
    whereas the old one used "sequential_table". Thus this query needs to be
    updated.
    
    --
    
    [1] - https://github.com/nvim-treesitter/nvim-treesitter/commit/7d1bab65469bd53e7a68666de071d9a82a522a40
    [2] - https://github.com/alexmozaidze/tree-sitter-fennel

--- queries/fennel/sexp.scm
+++ queries/fennel/sexp.scm
@@ -1,6 +1,6 @@
 [
  (_ "(" @sexp.open (_)? @sexp.elem ")" @sexp.close)
- (sequential_table "[" @sexp.open (_)? @sexp.elem "]" @sexp.close)
+ (sequence "[" @sexp.open (_)? @sexp.elem "]" @sexp.close)
  (table "{" @sexp.open (_)? @sexp.elem "}" @sexp.close)
  ] @sexp.form
 (program (_) @sexp.elem)
